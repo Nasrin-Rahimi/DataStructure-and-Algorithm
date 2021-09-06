8783e7e9265962cc2379d6f5bdd5c9331ed37eb3
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-09-06 07:53:39 +0430

    check a string for balance parantheses

---
+++ check-balanced-parentheses.js
@@ -0,0 +1,37 @@
+//Write a Parentheses Checker function to determine if the 
+//input string’s opening and closing brackets are properly nested.
+//"{[]()}"  => true
+//"{(()}"   => false
+
+function balancedParentheses(str) {
+    let stack = [];
+    let map = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
+
+    for(let i = 0; i < str.length; i++) {
+        // If character is an opening brace add it to a stack
+        if(str[i] === '(' || str[i] === '[' || str[i] === '{') {
+            stack.push(str[i]);
+        }
+        //if closing brace, pop from stack
+        else {
+            let openBrace = stack.pop();
+            if(str[i] !== map[openBrace]) {
+                return false;
+            }
+
+        }
+    }
+
+    if(stack.length !== 0) {
+        return false;
+    }
+    return true;
+}
+
+console.log("Does {[]()} have balanced parantheses?",balancedParentheses("{[]()}" ));
+console.log("Does {[(])} have balanced parantheses?", balancedParentheses("{[(])}"));
+console.log("Does {[} have balanced parantheses?", balancedParentheses("{[}"));
