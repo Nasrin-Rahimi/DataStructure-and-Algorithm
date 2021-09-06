55eb2a88656e7c60ff7b8248636f0709b3b4ee87
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-09-06 10:28:35 +0430

    destructuring undefined in js

--- array-destructuring.js
+++ array-destructuring.js
@@ -4,3 +4,23 @@ function removeFirstTwo(myArray) {
     const[, , ...newArray] = myArray;
     return newArray;
 }
+
+//destructure undefined in JavaScript
+// input in the point variable, and we want the output values of
+// name and age properties.
+
+function pointValues(point) {
+    const {name: a, age: b} = {...point};
+    console.log("name of point is: ", a);
+    console.log("age of point is: ", b);
+}
+
+pointValues({name: "Nasrin", age: 40});
+pointValues(undefined);
+/*
+We do that using {...point}, which creates a new object using the properties 
+of point. Now, the new object will contain all the same values as a kind of
+ copy. The values undefined and null are ignored. When undefined is spread, 
+ no value is stored in the new object, so no error is thrown. Thus, 
+ we see undefined when we access name and age.
+*/
