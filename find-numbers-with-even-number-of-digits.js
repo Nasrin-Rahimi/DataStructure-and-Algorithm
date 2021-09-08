6513b9bc77b7f476bed21b7849b7b3aadcaa5b08
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-09-08 16:45:50 +0430

    find number in array with even digit number

---
+++ find-numbers-with-even-number-of-digits.js
@@ -0,0 +1,49 @@
+/*
+Given an array nums of integers, return how many of them contain an even number of digits.
+
+Input: nums = [12,345,2,6,7896]
+Output: 2
+12 and 7896 contain an even number of digits.
+
+*/
+
+let findNumbers = function(nums) {
+    let count = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+        let len = nums[i].toString().length;
+        //OR if(getDigitCount(nums[i]) % 2 === 0)
+        if(parseInt(len) % 2 === 0)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+//OR Divide the number by 10 again and again to get the number of digits.
+let findNumbers2 = function(nums) {
+    let count = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+       
+        if(getDigitCount(nums[i]) % 2 == 0){
+            count = count++;
+        }
+    }
+    //return count;
+}
+//Divide the number by 10 again and again to get the number of digits.
+function getDigitCount(n) {
+    let i = parseInt(n / 10);
+    if(i === 0) {
+        return 1;
+    }
+    else {
+        return getDigitCount(i) + 1;
+    }
+}
+
+
+console.log(findNumbers2([12]));
