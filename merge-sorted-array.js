1cf20d8f6f81a66225803f1ddba4b72f4a15d53e
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-09-11 06:17:27 +0430

    another approch of merge with a third helper array

--- merge-sorted-array.js
+++ merge-sorted-array.js
@@ -40,14 +40,17 @@ let merge = function(nums1, m, nums2, n) {
 Time complexity : \mathcal{O}((n + m)\log(n + m))O((n+m)log(n+m)).
 
 The cost of sorting a list of length xx using a built-in sorting algorithm
- is \mathcal{O}(x \log x)O(xlogx). Because in this case we're sorting a list of length 
-m + nm+n, we get a total time complexity of \mathcal{O}((n + m) \log (n + m))O((n+m)log(n+m)).
-Space complexity : \mathcal{O}(n)O(n), but it can vary.
+ is (xlogx). Because in this case we're sorting a list of length 
+m + nm+n, we get a total time complexity of O((n+m)log(n+m)).
+Space complexity : O(n), but it can vary.
 
-Most programming languages have a built-in sorting algorithm that uses \mathcal{O}(n)O(n)
+Most programming languages have a built-in sorting algorithm that uses O(n)
 space.
 */
 
+/*
+Approch 2
+*/
 let merge2 = function(nums1, m, nums2, n) {
     if(n == 0) {
         return nums1;
@@ -80,3 +83,50 @@ let merge2 = function(nums1, m, nums2, n) {
     }
     return nums1;
 };
+
+//time complexity : O(m + n)(m + n)
+
+/*
+Approach 2 : Three Pointers (Start From the Beginning)
+Because each array is already sorted, we can achieve an O(n+m)
+ time complexity with the help of the two-pointer technique.
+
+ Algorithm
+
+The simplest implementation would be to make a copy of the values in nums1, called 
+nums1Copy, and then use two read pointers and one write pointer to read values from
+ nums1Copy and nums2 and write them into nums1.
+
+Initialize nums1Copy to be a new array containing the first m values of nums1.
+Initialize read pointer p1 to the beginning of nums1Copy.
+Initialize read pointer p2 to the beginning of nums2.
+Initialize write pointer p to the beginning of nums1.
+While p is still within nums1:
+If nums1Copy[p1] exists and is less than or equal to nums2[p2]:
+Write nums1Copy[p1] into nums1[p], and increment p1 by 1.
+Else
+Write nums2[p2] into nums1[p], and increment p2 by 1.
+Increment p by 1.
+
+*/
+
+let merge3 = function(nums1, m, nums2, n) {
+    let nums1Copy = new Array(m);
+    
+    for(let i = 0; i < m; i++) {
+        nums1Copy[i] = nums1[i];
+    }
+    
+    let p1 = 0, p2 = 0, p = 0;
+
+    while (p < m + n) {
+        if(p2 >= n || p1 < m && nums1Copy[p1] <= nums2[p2]) {
+            nums1[p] = nums1Copy[p1];
+            p1++;
+        } else {
+            nums1[p] = nums2[p2];
+            p2++
+        }
+        p++;
+    }
+}
