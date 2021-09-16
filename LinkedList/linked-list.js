648100abcbab332c675064259fc04363d3dcb364
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-09-16 11:06:16 -0400

    create a linked list class

---
+++ LinkedList/linked-list.js
@@ -0,0 +1,17 @@
+class ListNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class MyLinkedList {
+    constructor() {
+        this.head = null; // Sentinel node as pseudo-head
+        this.size = 0;
+    }
+
+    addAtHead(val) {
+
+    }
+ }
