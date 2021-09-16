af92050e9d87e9f6c4cf4bcb599d73317ccc7170
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-09-16 12:05:31 -0400

    check if the linked list is empty

--- LinkedList/linked-list.js
+++ LinkedList/linked-list.js
@@ -90,130 +90,18 @@ class MyLinkedList {
         }
         this.size--;
     }
+
+    isEmpty() {
+        return this.size === 0;
+    }
  }
 
 
 
-//  class Node {
-//     constructor(element) {
-//         this.element = element;
-//         this.next = null;
-//     }
-// }
 
-// class LinkedList {
-//     constructor() {
-//         this.head = null;
-//         this.size = 0;
-//     }
 
-//     //add element to the end of linked list
-//     add(element) {
-//         //create a new node
-//         let node = new Node(element);
 
-//         //if list is empty, add element and make it head.
-//         if(this.head == null){
-//             this.head = node;
-//         } else {
-//             let current = this.head;
 
-//             while(current.next) {
-//                 current = current.next;
-//             }
-//             //add node
-//             current.next = node;
-//         }
-//         this.size++;
-//     }
-
-//     insertAt(element, index) {
-//         if(index < 0 || index > this.size) {
-//             return console.log("Please enter a valid index!");
-//         } else {
-//             let node = new Node(element);
-
-//             if(index == 0) {
-//                 node.next = this.head;
-//                 this.head = node;
-//             } else {
-//                 let curr = this.head;
-//                 let prev;
-//                 let i = 0;
-//                 while (i < index) {
-//                     prev = curr;
-//                     curr = curr.next;
-//                     i++;
-//                 }
-
-//                 node.next = curr;
-//                 prev.next = node;
-//             }
-//             this.size++;
-//         }
-//     }
-
-//     removeFrom(index) {
-//         if(index < 0 || index > this.size) {
-//             return console.log("Please enter a valid index!");
-//         } else {
-//             let prev, curr, i = 0;
-//             curr = this.head;
-
-//             if(index == 0) {
-//                 this.head = curr.next;
-//             } else {
-//                 while (i < index) {
-//                     prev = curr;
-//                     curr = curr.next;
-//                     i++;
-//                 }
-//                 prev.next = curr.next;
-//             }
-//             this.size--;
-//             return curr.element;
-//         }
-//     }
-
-//     removeElement(element){
-//         let current = this.head;
-//         let prev = null;
-
-//         while(current != null) {
-//             if(current.element == element) {
-//                 if(prev == null){
-//                     this.head = current.next;
-//                 } else {
-//                     prev.next = current.next;
-//                 }
-//                 this.size--;
-//                 return current.element;
-//             }
-//             prev = current;
-//             current = current.next;
-//         }
-//         return -1;
-//     }
-
-//     indexOf(element) {
-//         let count = 0;
-//         let current = this.head;
-
-//         while(current != null) {
-//             if(current.element == element) {
-//                 return count;
-//             }
-//             count++;
-//             current = current.next;
-//         }
-
-//         //element not found
-//         return -1;
-//     }
-
-//     isEmpty(){
-//         return this.size == 0;
-//     }
 
 //     listSize(){
 //         return this.size;
