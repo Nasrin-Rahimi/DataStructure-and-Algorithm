d6815101b2fb913afd1b7f5ed5789c17e190ecf4
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-09-22 15:43:20 -0400

    climb stairs dynamic programming

--- Recursion/climbing-stairs.js
+++ Recursion/climbing-stairs.js
@@ -91,3 +91,48 @@ let recursionHelper2 = function(i, n, memo){
 
 Space complexity : O(n). The depth of recursion tree can go upto n.
  */
+
+/**
+ * Approach 3: Dynamic Programming
+ * As we can see this problem can be broken into subproblems, and it contains the optimal 
+ * substructure property i.e. its optimal solution can be constructed efficiently from 
+ * optimal solutions of its subproblems, we can use dynamic programming to solve this problem.
+
+One can reach i^{th}
+  step in one of the two ways:
+
+Taking a single step from (i−1)th
+  step.
+
+Taking a step of 22 from (i−2)th
+  step.
+
+So, the total number of ways to reach ith
+  is equal to sum of ways of reaching (i−1)th step and ways of reaching (i−2) th step.
+
+Let dp[i] denotes the number of ways to reach on ithstep:
+
+dp[i]=dp[i-1]+dp[i-2]
+*/
+
+let climbStairs3 = function(n) {
+    if(n == 1) {
+        return 1;
+    }
+
+    let dp = new Array(n + 1);
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for(let i = 3; i < n + 1; i++){ 
+        dp[i] = dp[i - 1] + dp[i -2];
+    }
+
+    return db[n];
+}
+
+/**
+ * Time complexity : O(n). Single loop upto n.
+
+Space complexity : O(n). dp array of size n is used.
+ */
