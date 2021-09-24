ac8d2a0aa712c6d61b2b80e2a60d80b9ef8b0549
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-09-24 16:25:15 -0400

    two sum by hash table and one pass

--- Array/two-sum.js
+++ Array/two-sum.js
@@ -89,5 +89,34 @@ let twoSum2 = function(nums, target) {
 Space complexity: (n). The extra space required depends on the number of items stored in the 
 hash table, which stores exactly nn elements.
 
+ */
 
+/**
+ * Approach 3: One-pass Hash Table
+ * It turns out we can do it in one-pass. While we are iterating and inserting elements into the 
+ * hash table, we also look back to check if current element's complement already exists in the 
+ * hash table. If it exists, we have found a solution and return the indices immediately.
+
+ */
+
+let twoSum3 = function(nums, target) {
+    let map = new Map();
+
+    for(let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if(map.has(complement)) {
+            return [map.get(complement), i];
+        } else {
+            map.set(nums[i], i);
+        }
+    }
+    return null;
+}
+
+/**
+ * Time complexity: O(n). We traverse the list containing nn elements only once. Each lookup 
+ * in the table costs only O(1) time.
+
+Space complexity: O(n). The extra space required depends on the number of items stored in the 
+hash table, which stores at most n elements.
  */
