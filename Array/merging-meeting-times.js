34896d6b2c2293c07dc08154ada660b884df219d
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-11-02 13:20:41 -0400

    stack frame

--- Array/merging-meeting-times.js
+++ Array/merging-meeting-times.js
@@ -3,7 +3,7 @@
  * to see the times in a day when everyone is available.
 
 To do this, you’ll need to know when any team is having a meeting. In HiCal, a meeting is 
-stored as objects ↴ with integer properties startTime and endTime. These integers represent 
+stored as objects(Helpers/object.md) with integer properties startTime and endTime. These integers represent 
 the number of 30-minute blocks past 9:00am.
 
 For example:
