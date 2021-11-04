49e37d9e4fad7fbc93d432fc389cebdb935c5522
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-11-04 10:14:04 -0400

    flight entertainment solution

--- Hash Table/inflight-intertainment.js
+++ Hash Table/inflight-intertainment.js
@@ -31,3 +31,98 @@ How would we implement this in code? We could nest two loops (the outer choosing
 firstMovieLength, the inner choosing secondMovieLength). That’d give us a runtime of 
 O(n^2). We can do better.
  */
+
+function canTwoMoviesFillFlight(movieLengths, flightLength) {
+    for(let i = 0; i < movieLengths; i++) {
+        for(let  j = 0;  j < movieLengths; j++) {
+            if(i !== j) {
+                if (movieLengths[j] = flightLength - movieLengths[i]) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+/**
+To bring our runtime down we'll probably need to replace that inner loop (the one that 
+looks for a matching secondMovieLength) with something faster.
+
+We could sort the movieLengths first—then we could use binary search ↴ to find 
+secondMovieLength in O(lgn) time instead of O(n) time. But sorting would cost O(nlg(n)), 
+and we can do even better than that.
+
+Could we check for the existence of our secondMovieLength in constant time?
+
+What data structure gives us convenient constant-time lookups?
+
+A set!
+
+So we could throw all of our movieLengths into a set first, in O(n) time. Then we could loop 
+through our possible firstMovieLengths and replace our inner loop with a simple check in 
+our set. This'll give us O(n) runtime overall!
+
+Of course, we need to add some logic to make sure we're not showing users the same movie twice...
+
+But first, we can tighten this up a bit. Instead of two sequential loops, can we do it 
+all in one loop? (Done carefully, this will give us protection from showing the same 
+movie twice as well.)
+ */
+
+/**
+Solution
+We make one pass through movieLengths, treating each item as the firstMovieLength. 
+At each iteration, we:
+
+See if there's a matchingSecondMovieLength we've seen already (stored in our 
+movieLengthsSeen set) that is equal to flightLength - firstMovieLength. 
+If there is, we short-circuit and return true.
+Keep our movieLengthsSeen set up to date by throwing in the current firstMovieLength.
+ */
+
+function canTwoMoviesFillFlight(movieLengths, flightLength) {
+
+    // Movie lengths we've seen so far
+    const movieLengthsSeen = new Set();
+  
+    for (let i = 0; i < movieLengths.length; i++) {
+      const firstMovieLength = movieLengths[i];
+  
+      const matchingSecondMovieLength = flightLength - firstMovieLength;
+      if (movieLengthsSeen.has(matchingSecondMovieLength)) {
+        return true;
+      }
+  
+      movieLengthsSeen.add(firstMovieLength);
+    }
+  
+    // We never found a match, so return false
+    return false;
+}
+
+/**
+We know users won't watch the same movie twice because we check movieLengthsSeen 
+for matchingSecondMovieLength before we've put firstMovieLength in it!
+
+Complexity
+O(n) time, and O(n) space. Note while optimizing runtime we added a bit of space cost.
+ */
+
+/**
+Bonus
+What if we wanted the movie lengths to sum to something close to the flight length 
+(say, within 20 minutes)?
+What if we wanted to fill the flight length as nicely as possible with any number of 
+movies (not just 2)?
+What if we knew that movieLengths was sorted? Could we save some space and/or time?
+ */
+
+/**
+What We Learned
+The trick was to use a set to access our movies by length, in O(1) time.
+
+Using hash-based data structures, like objects or sets, is so common in coding challenge 
+solutions, it should always be your first thought. Always ask yourself, right from the 
+start: "Can I save time by using an object?"
+ */
