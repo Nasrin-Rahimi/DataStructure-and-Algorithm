e7aaeb5cde08ee2c10c3d6c52fe03055f18cef4c
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-11-09 09:23:33 -0500

    highest product of 3 time complexity

--- Greedy Algorithms/highest-product-of-3.js
+++ Greedy Algorithms/highest-product-of-3.js
@@ -167,5 +167,45 @@ function highestProductOf3(arrayOfInts) {
 }
 
 /**
- * 
+Complexity
+O(n) time and O(1) additional space.
+ */
+
+/**
+Bonus
+What if we wanted the highest product of 4 items?
+What if we wanted the highest product of k items?
+If our highest product is really big, it could overflow. ↴ How should we protect against this?
+ */
+
+/**
+What We Learned
+Greedy ↴ algorithms in action again!
+
+That's not a coincidence—to illustrate how one pattern can be used to break down several 
+different questions, we're showing this one pattern in action on several different questions.
+
+Usually it takes seeing an algorithmic idea from a few different angles for it to really 
+make intuitive sense.
+
+Our goal here is to teach you the right way of thinking to be able to break down problems 
+you haven't seen before. Greedy algorithm design is a big part of that way of thinking.
+
+For this one, we built up our greedy algorithm exactly the same way we did for the Apple 
+stocks question. By asking ourselves:
+
+"Suppose we could come up with the answer in one pass through the input, by simply 
+updating the 'best answer so far' as we went. What additional values would we need to 
+keep updated as we looked at each item in our set, in order to be able to update the 
+'best answer so far' in constant time?"
+
+For the Apple stocks question, the only "additional value" we needed was the min price so far.
+
+For this one, we needed four things in order to calculate the new highestProductOf3 at 
+each step:
+
+highestProductOf2
+highest
+lowestProductOf2
+lowest
  */
