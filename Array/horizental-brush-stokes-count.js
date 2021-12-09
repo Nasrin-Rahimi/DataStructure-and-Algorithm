a72d3ed45aeacd49d6cff3a3b929a5f94133b2a8
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-12-09 09:39:28 -0500

    number of brush strokes for paiting, effiteint solution

--- Array/horizental-brush-stokes-count.js
+++ Array/horizental-brush-stokes-count.js
@@ -1,9 +1,10 @@
 /**
 By given an array of integers, each element represents a building. For example: 
-int buildings[] = {1, 4, 3, 2, 3, 1}.
+let buildings = [1, 4, 3, 2, 3, 1].
 
 If we drew the buildings horizontally with a brush, how many brush strike we would use?
 
+The function should return -1 if the number of strokes exceeds 100000000.
  */
 
 /**
@@ -14,4 +15,31 @@ The external loop running on the levels of each building (according to the highe
 
 The inner loop is running on the array from 0 to n, and compares the difference of the height (0 or 1) 
 between two near elements.
+ */
+
+/**
+Efficeint Solution: 
+A brush stroke starts whenever the height increases going from left to right, and ends when it 
+decreases. We only need to look at when it increases, because if we just count the starting points 
+of each stroke we will have the stroke count. Instead of looping over the height levels in an inner 
+loop, just subtract one height level from the previous to get the difference.
+
+ */
+
+function solution(buildings) {
+    let brushCount = 0;
+    let previousHeight = 0;
+
+    for(let i = 0; i < buildings.length; i++) {
+        if(buildings[i] > previousHeight) {
+            brushCount = brushCount + (buildings[i] - previousHeight);
+        }
+        previousHeight = buildings[i];
+    }
+
+    return brushCount <= 100000000 ? brushCount : -1;
+}
+
+/**
+Time Complexity = O(n)
  */
