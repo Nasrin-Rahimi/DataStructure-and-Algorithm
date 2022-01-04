e6d7f067e6cf3ecb66f8c70c3e6627a33afb19d1
Nasrin-Rahimi <77.nasrin@gmail.com>
2022-01-04 09:50:17 -0500

    create some dvds and add them to the array

--- Array/creating-array.js
+++ Array/creating-array.js
@@ -1,6 +1,5 @@
 
 class Dvd {
-
     constructor(name, director, releaseYear){
         this.name = name;
         this.director = director;
@@ -14,6 +13,25 @@ class Dvd {
 }
 
 const dvdCollection = new Array(15);
+//create a new dvd
+const ebiDvd = new Dvd('Khatereh', 'Ebi', '1990');
+//put the dvd in to the array
+dvdCollection[0] = ebiDvd;
 
-dvdCollection[0] = new Dvd('Khatereh', 'Ebi', '1990');
+const incrediblesDvd = new Dvd("The Incredibles", 2004, "Brad Bird");
+const findingDoryDvd = new Dvd("Finding Dory", 2016, "Andrew Stanton");
+const lionKingDvd = new Dvd("The Lion King", 2019, "Jon Favreau");
+
+// Put "The Incredibles" into the 4th place: index 3.
+dvdCollection[3] = incrediblesDvd;
+
+// Put "Finding Dory" into the 10th place: index 9.
+dvdCollection[9] = findingDoryDvd;
+
+// Put "The Lion King" into the 3rd place: index 2.
+dvdCollection[2] = lionKingDvd;
+
 console.log(dvdCollection[0].toString());
+console.log(dvdCollection[3].toString());
+console.log(dvdCollection[9].toString());
+console.log(dvdCollection[2].toString());
