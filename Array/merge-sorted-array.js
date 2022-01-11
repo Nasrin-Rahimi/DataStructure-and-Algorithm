b613ced2698fb365eb488ddbe0092c9c284f156f
Nasrin-Rahimi <77.nasrin@gmail.com>
2022-01-11 13:49:17 -0500

    error debuging

--- Array/merge-sorted-array.js
+++ Array/merge-sorted-array.js
@@ -33,7 +33,7 @@ let merge = function(nums1, m, nums2, n) {
     for(let i = 0; i < n; i++) {
         nums1[i + m] = nums2[i];
     }
-    return nums1.sort();
+    return nums1.sort((a, b) => a - b);
 }
 
 /** 
