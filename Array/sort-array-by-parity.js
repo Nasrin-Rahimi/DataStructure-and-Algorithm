4f965ed4d1b68444f33e4406fa164887cc0e88e6
Nasrin-Rahimi <77.nasrin@gmail.com>
2022-01-12 09:49:44 -0500

    clean code

--- Array/sort-array-by-parity.js
+++ Array/sort-array-by-parity.js
@@ -40,20 +40,19 @@ Approch 2 : Use 2 pointer
 */
 
 let sortArrayByParity2 = function(nums) {
-    let len = nums.length;
-    let i = 0, j = len - 1;
-    
-    while(i < j) {
-        if(nums[i] % 2 === 0) {
-            i++;
+    let left = 0, right = nums.length - 1;
+
+    while(left < right) {
+        if(nums[left] % 2 === 0) {
+            left++;
+        } else if(nums[right] % 2 !== 0) {
+            right--;
         } else {
-            if(nums[j] % 2 === 0) {
-                let tmp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = tmp;
-                i++;
-            }
-            j--;
+            const temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
         }
     }
     
