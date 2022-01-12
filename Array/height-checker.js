20442c7d914629eb43f007339ce6934623071f69
Nasrin-Rahimi <77.nasrin@gmail.com>
2022-01-12 18:55:18 -0500

    hieght checker using map

--- Array/height-checker.js
+++ Array/height-checker.js
@@ -32,6 +32,11 @@ Explanation:
 heights:  [1,2,3,4,5]
 expected: [1,2,3,4,5]
 All indices match.
+
+Constraints:
+
+1 <= heights.length <= 100
+1 <= heights[i] <= 100
  */
 
 /**
@@ -39,6 +44,7 @@ Approch 1: Using extra space
  */
 let heightChecker = function(heights) {
     let expected = JSON.parse(JSON.stringify(heights));
+    //or let expected = [...heights].sort((a, b) => a - b)
     let indices= 0;
 
     expected = expected.sort((a,b) => a - b);
@@ -55,4 +61,42 @@ let heightChecker = function(heights) {
 /**
  * Time complexity: O(nlogn) for sort + O(n) for for loop to comparing heights. So it would 
  * equal O(nlogn) where n is the size of heights array.
+ * Space complexity: O(n) for expected array.
  */
+
+/**
+Approch 2: Because it is heights we are talking about (which naturally have a limit) and the 
+problem gave us that heights is between 1 and 100, we can actually do a bucket/counting sort 
+which only takes O(100n) = O(n) time.
+
+The answer first counts up the heights and puts them in a map and then compares them in 
+order to the heights in the input array.
+ */
+
+function heightChecker(heights) {
+    let map = {};
+    let h_ptr = 101;
+    // create map of all heights occurences
+    for(let h of heights) {
+        if (h < h_ptr) {
+            h_ptr = h;
+        }
+        map[h]? ++map[h]:map[h]=1;
+    }
+    let res = 0;
+ 
+    for(let h of heights) {
+        // find first height in map
+        while(map[h_ptr] === undefined || map[h_ptr] ===0 ) ++h_ptr;
+ 
+        // if height is not matching then the position is wrong
+        if(h_ptr != h) {
+            ++res;
+        }
+ 
+        // reduce count of height in map
+        --map[h_ptr];
+    }
+ 
+    return res;
+ }
