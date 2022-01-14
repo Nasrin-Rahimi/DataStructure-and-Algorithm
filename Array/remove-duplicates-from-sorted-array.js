488452333769461978ddbe5b17797605e728c076
Nasrin-Rahimi <77.nasrin@gmail.com>
2022-01-14 08:46:22 -0500

    remove unnecessary comments

--- Array/remove-duplicates-from-sorted-array.js
+++ Array/remove-duplicates-from-sorted-array.js
@@ -1,7 +1,3 @@
-/**
- * @typedef {*} NewType
- */
-
 /** 
 Given an integer array nums sorted in non-decreasing order, remove the duplicates 
 in-place such that each unique element appears only once. The relative order of the
