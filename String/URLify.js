bfc1500c55fc2746676a7f04be187db939dbd924
Nasrin-Rahimi <77.nasrin@gmail.com>
2022-01-19 10:55:41 -0500

    urlify if we have unlimited spaces at the end of string

--- String/URLify.js
+++ String/URLify.js
@@ -28,7 +28,7 @@ from the end ,and working backword. This is useful because we have extra buffer 
 end, which allows us to change characters without worrying about what we're overwriting.
  */
 
-function replaceSpaces(str, trueLength) {
+function replaceSpaces1(str, trueLength) {
 
     let strArray = str.split('');
 
@@ -52,7 +52,7 @@ function replaceSpaces(str, trueLength) {
     
 }
 
-console.log(replaceSpaces('Mr John Smith    ', 13));
+// console.log(replaceSpaces('Mr John Smith    ', 13));
 /**
  * Time Complexity : O(n)
  * Space Complexity: O(n) for the strArray
@@ -64,3 +64,49 @@ console.log(replaceSpaces('Mr John Smith    ', 13));
  * the array unntil we get to the last space. After that we don't need to continue to loop
  * because every character is in the right place already.
  */
+
+/** But what if we have unlimited space at the end of the string? 
+ * First we count the number of spaces. We need two extra characetrs for each space
+ * (since '%20' is two characters longer than ' ') so we double this count. Then, we walk
+ * backwards though the string, editing it. When we see a space, we replace it with '%20'
+ * If there is no space, then we copy the original character.
+ * we have implemented this using char array, because js strings are immutable.
+*/
+
+function replaceSpaces(str, trueLength) {
+    let spaceCount = countOfChar(str, 0, trueLength, ' ');
+    let last = trueLength - 1;
+    let newLen = trueLength + (spaceCount * 2);
+    let i = newLen - 1;
+    let strArray = str.split('');
+
+    while( spaceCount > 0) {
+        if(strArray[last] === ' ') {
+            strArray[i] = '0';
+            strArray[i - 1] = '2';
+            strArray[i - 2] = '%';
+            i -= 3;
+            spaceCount--;
+        } else {
+            strArray[i] = strArray[last];
+            i--;
+        }
+        last--;
+    }
+
+    return strArray.join('');
+}
+
+function countOfChar(str, start, end, target) {
+    let count = 0;
+
+    for(let i = start; i < end; i++) {
+        if(str[i] === target) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+console.log(replaceSpaces('Mr John Smith    ', 13));
