55e8f2f62901f309e6c7fafccc62f2e15504b140
Nasrin-Rahimi <77.nasrin@gmail.com>
2022-01-27 11:08:31 -0500

    test the server channnel with gready algorithm

--- Array/test-the-server-channels.js
+++ Array/test-the-server-channels.js
@@ -1,5 +1,5 @@
 /**
-Amazon Developers want to do stress test to check the quality of servers' channels. 
+Company's Developers want to do stress test to check the quality of servers' channels. 
 They must ensure the following
 
 1- Each of the packets must be sent via a single channel
@@ -33,9 +33,64 @@ Please help out with solution.
 
 /**
 Solution 1: 
-1- Sort the item.
-2- Add the item in one variable up till channel -1
-    a. Add the item from last and remove from the list
-3- Then just calculate the standard median of the remaining array.
-4- Add the median and you are done.
+I am trying out a greedy approach here but I can't prove it so i dont know if this solution 
+ is right or wrong. What you basically need to do is first sort the array. Then take 
+ (Channels - 1) greatest elements from the array. And from the remaining array take the 
+ median if its length > 1.
+ 
+1- Sort the packets array.
+2- If we have k channels, put the packets with highest quantity, one per channel in 
+k - 1 channels. In this way we sum up the highest numbers and get the maximum possible sum 
+of the quantities.
+for doing this, iterate over the packets from last to first packet and add up packets to 
+one variable. 
+3- Put the remaining packets in the last channel. 
+Just calculate the median of the remaining packets (the packets with lowest quantity).
+4- Add the median to the variable that sum up other packets and we are done.
  */
+
+
+function getMaximumQuality (packets, channels) {
+
+    if(packets == null || packets.length == 0) {
+        return 0;
+    }
+
+    if (channels <= 0) {
+        throw new Error("You don't have any channel to send packets!");
+    }
+
+    let n = packets.length;
+    let i = n - 1;
+    let maxSum = 0;
+    //The channels are more than packets.(every channet must transfer at least one packet)
+    if(channels > n) {
+        throw new Error('We have at least one chennet without any packet!');
+    }
+
+    if(n === 1) {
+        return packets[0];
+    }
+
+    packets.sort((a,b) => a - b);
+
+    while(i >= n - channels + 1) {
+        maxSum += packets[i];
+        i--;
+    }
+    
+    const remainingPacketsCount = i + 1;
+    //calculate the median of the remaining packets for the last channel
+    if(remainingPacketsCount % 2 === 0) { //the number of remaining packets are even
+        const median = (packets[parseInt(remainingPacketsCount / 2)] 
+        + packets[parseInt(remainingPacketsCount / 2) - 1]) / 2 ;
+        maxSum += median;
+    } else {
+        maxSum += packets[parseInt(remainingPacketsCount / 2)];
+    }
+
+    return Math.ceil(maxSum);
+
+}
+
+console.log(getMaximumQuality([1,2,3,4,5], 1));
