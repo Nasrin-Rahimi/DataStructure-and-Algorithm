93402577ba919c4c0c7be3484a13c075e563c1e3
Nasrin-Rahimi <77.nasrin@gmail.com>
2022-01-28 11:07:44 -0500

    create phone string by slice and join

--- Array/create-phone-number.js
+++ Array/create-phone-number.js
@@ -26,3 +26,29 @@ Time complexity: O(n^2) because string concatination in each iteration takes O(n
 concatination create a new string and copy the original string in the new string and add
 to the new string.
  */
+
+/**
+ * Solution 2:
+we're going to use the .slice and .join methods in order to take out the values that we 
+need from the array between the specified indexes. Also we'll use the string 
+template literals in order to have a cleaner code:
+ */
+
+function createPhoneNumber(arr) {
+    return `(${arr.slice(0, 3).join('')}) ${arr.slice(3, 6).join('')}-${arr.slice(6).join('')}`;
+}
+
+/**
+ * This solution is more clean than the first one. 
+ * Time complexity: O(n) (Not sure)
+ */
+
+/**
+Or we can create a string at the beginning by .joininig the characters and then using 
+.substring to take the specific ones out:
+ */
+
+function createPhoneNumber(arr) {
+    let str = arr.join('');
+    return `(${str.substring(0, 3)}) ${str.substring(3, 6)}-${str.substring(6)}`;
+}
