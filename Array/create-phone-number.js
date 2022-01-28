9b21110830637bcfb63c144d33510ddd34cb09b6
Nasrin-Rahimi <77.nasrin@gmail.com>
2022-01-28 14:24:36 -0500

    error debugging

--- Array/create-phone-number.js
+++ Array/create-phone-number.js
@@ -6,20 +6,24 @@ Define a function, createPhoneNumber, that accepts an array of 10 integers
 /**
 Solution 1: 
  */
-function createPhoneNumber(phoneNoArr) {
+function createPhoneNumber1(phoneNoArr) {
     let phoneNoStr = "";
     for(let i = 0; i < phoneNoArr.length; i++) {
-        if(i === 3 || i === 6){
-            phoneNoStr = phoneNoStr + "-" + phoneNoArr[i];
+        if(i === 0) {
+            phoneNoStr += '(' + phoneNoArr[i];
+        } else if(i === 2) {
+            phoneNoStr += phoneNoArr[i] + ')';
+        } else if(i === 5) {
+            phoneNoStr += phoneNoArr[i] + '-';
         }
         else {
             phoneNoStr += phoneNoArr[i];
         }
     }
-    console.log(phoneNoStr);
+    return phoneNoStr;
 }
 
-//createPhoneNumber([0,1,2,3,4,5,6,7,8,9]);
+console.log(createPhoneNumber1([6,8,1,2,8,5,9,1,0,8]));
 
 /**
 Time complexity: O(n^2) because string concatination in each iteration takes O(n)
@@ -51,4 +55,22 @@ Or we can create a string at the beginning by .joininig the characters and then 
 function createPhoneNumber(arr) {
     let str = arr.join('');
     return `(${str.substring(0, 3)}) ${str.substring(3, 6)}-${str.substring(6)}`;
+}
+
+/**
+Solution 3:
+For this one we'll create a mask that will be used to .replace (insert) the numbers inside 
+the string. Note that the .replace function will find and replace only the first occurrence 
+of x in this case (which is what we want), then it will move on to the next x in the next 
+iteration and so on...
+ */
+
+function createPhoneNumber(arr) {
+    let mask = '(xxx) xxx-xxxx';
+
+    arr.forEach(item => {
+        mask = mask.replace('x', item);
+    });
+
+    return mask;
 }
