8d5577825c08e5e85b618708f117a8a671b71db2
Nasrin-Rahimi <77.nasrin@gmail.com>
2022-01-28 10:59:26 -0500

    brute force time complexity for create phone number

--- Array/create-phone-number.js
+++ Array/create-phone-number.js
@@ -1,8 +1,11 @@
-/*
+/** 
 Define a function, createPhoneNumber, that accepts an array of 10 integers
  (from 0-9) and returns a string of those numbers in the form of a phone number.
 */
 
+/**
+Solution 1: 
+ */
 function createPhoneNumber(phoneNoArr) {
     let phoneNoStr = "";
     for(let i = 0; i < phoneNoArr.length; i++) {
@@ -16,4 +19,10 @@ function createPhoneNumber(phoneNoArr) {
     console.log(phoneNoStr);
 }
 
-createPhoneNumber([0,1,2,3,4,5,6,7,8,9]);
+//createPhoneNumber([0,1,2,3,4,5,6,7,8,9]);
+
+/**
+Time complexity: O(n^2) because string concatination in each iteration takes O(n)
+concatination create a new string and copy the original string in the new string and add
+to the new string.
+ */
