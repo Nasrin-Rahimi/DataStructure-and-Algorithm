3c59df07c53b02d24eb8b8a364c6476e2e4eb34a
Nasrin-Rahimi <77.nasrin@gmail.com>
2022-01-31 09:30:46 -0500

    add example for more clarification

--- Array/array-destructuring.js
+++ Array/array-destructuring.js
@@ -9,18 +9,32 @@ function removeFirstTwo(myArray) {
 // input in the point variable, and we want the output values of
 // name and age properties.
 
-function pointValues(point) {
+function pointValuesByDestructuring(point) {
     const {name: a, age: b} = {...point};
     console.log("name of point is: ", a);
     console.log("age of point is: ", b);
 }
 
-pointValues({name: "Nasrin", age: 40});
-pointValues(undefined);
-/*
+pointValuesByDestructuring({name: "Nasrin", age: 40});
+pointValuesByDestructurings(undefined);
+
+/** 
 We do that using {...point}, which creates a new object using the properties 
 of point. Now, the new object will contain all the same values as a kind of
  copy. The values undefined and null are ignored. When undefined is spread, 
  no value is stored in the new object, so no error is thrown. Thus, 
  we see undefined when we access name and age.
 */
+
+//consider the below function
+function pointValues(point) {
+    console.log("name of point is: ", point.name);
+    console.log("age of point is: ", point.age);
+}
+
+pointValues(undefined);
+
+/**
+ Calling pointValues with undefined, return Error TypeError
+ (Cannot read properties of undefined (reading 'name'))
+ */
