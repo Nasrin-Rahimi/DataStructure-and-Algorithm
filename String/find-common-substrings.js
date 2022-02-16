c2f534638729b83b93d595c4536e452cdfbd8561
Nasrin-Rahimi <77.nasrin@gmail.com>
2022-02-15 19:59:51 -0500

    find common substring

---
+++ String/find-common-substrings.js
@@ -0,0 +1,56 @@
+/**
+Given two arrays of strings, determine whether corresponding elements contain a common substring.
+
+Example: 
+a = ['ab', 'cd', 'ef']
+b = ['af', 'ee', 'ef']
+
+Make the following decision:
+
+i   a[i]    b[i]    common  result
+0   ab      af      a       YES
+1   cd      ee              NO
+2   ef      ef      ef      YES
+
+For each test, print the result on a new line. either YES if there is a common substring or NO.
+ */
+
+/**
+Solution : Walk through the arrays and in each step check if the corrosponding elements in two arrays
+have common substring, print YES to output otherwise print No to output.
+for  check if the corrosponding elements in two arrays have common substring, we use a helper method
+that accept two string, and create a hashtabel, then walk through the frist string and add the all 
+chars of string two a set, (we know set only save uniq items) then walk through the other string,
+and see if set has any char of that string, then we return true.
+
+ */
+
+function commonSubstrings(a, b) {
+    if(a.length == 0 || b.length == 0) {
+        throw new Error('One or two arrays is/are empty!');
+    }
+
+    for(let i = 0; i < a.length; i++) {
+        if(hasCommonSubstring(a[i], b[i])) {
+            console.log('YES');
+        } else {
+            console.log('NO');
+        }
+    }
+}
+
+function hasCommonSubstring(str1, str2) {
+    const seenChars = new Set();
+
+    for(let i = 0; i < str1.length; i++) {
+        seenChars.add(str1[i]);
+    }
+
+    for(let i = 0; i < str2.length; i++) {
+        if(seenChars.has(str2[i])) {
+            return true;
+        }
+    }
+
+    return false;
+}
